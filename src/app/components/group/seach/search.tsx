6ed6a9040fc8fa7948e846706a2ec2b15c40b4bd
tiago <ti.almeida58@gmail.com>
2025-02-04 13:36:36 -0300

    feat: creating search for page group

---
+++ src/app/components/group/seach/search.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import React, {useState} from 'react';
+import styles from './seach.module.css';
+import Input from "@/app/components/input/input";
+import Select from "@/app/components/select/select";
+
+const statusOptions = [
+    { value: 'ativo', label: 'Ativo' },
+    { value: 'desativado', label: 'Desativado' },
+    // Add more status options here
+];
+
+const Search = () => {
+
+    const [status, setStatus] = useState('ativo');
+
+    const handleStatusChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setStatus(event.target.value);
+    };
+
+    return (
+        <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <Input label={'Nome'} />
+                <Select
+                    label={'Status'}
+                    options={statusOptions}
+                    value={status}
+                    onChange={handleStatusChange}
+                />
+            </div>
+        </div>
+    );
+};
+
+export default Search;
