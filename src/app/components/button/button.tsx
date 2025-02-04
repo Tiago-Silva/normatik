f0a8a89a00c0180f8738baceb893a3e725f4312b
tiago <ti.almeida58@gmail.com>
2025-02-04 14:34:34 -0300

    feat: creating button component

---
+++ src/app/components/button/button.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { IconType } from 'react-icons';
+import styles from './button.module.css';
+
+interface Props {
+    title: string;
+    icon: IconType;
+    background?: string;
+    width?: string;
+}
+
+const Button: React.FC<Props> = (
+    {
+        title,
+        icon: Icon,
+        background = '#007bff',
+        width = '150px'
+    }) => {
+    return (
+        <button className={styles.container} style={{ background, width }}>
+            <Icon className={styles.icon} />
+            {title}
+        </button>
+    );
+};
+
+export default Button;
