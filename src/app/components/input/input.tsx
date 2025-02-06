25ade0ae7dc9bfbd9adb051de403d843450689bf
tiago <ti.almeida58@gmail.com>
2025-02-06 17:04:29 -0300

    refactor: adding onChange

--- src/app/components/input/input.tsx
+++ src/app/components/input/input.tsx
@@ -3,14 +3,21 @@ import styles from './input.module.css';
 
 interface Props {
     label: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
     width?: string;
 }
 
-const Input: React.FC<Props> = ({ label, width, ...res }) => {
+const Input: React.FC<Props> = ({ label, value, onChange,  width }) => {
     return (
         <div className={styles.inputContainer}>
             <label className={styles.label}>{label}</label>
-            <input className={styles.input} style={{ width }} {...res} />
+            <input
+                className={styles.input}
+                style={{ width }}
+                value={value}
+                onChange={onChange}
+            />
         </div>
     );
 };
