e0e2bbf036e261a2d4a5d3bac574fadaffe69c2e
tiago <ti.almeida58@gmail.com>
2025-02-11 10:01:41 -0300

    refactor: organizing the code, applying responsiveness and inputs

--- src/app/components/company/header/index.tsx
+++ src/app/components/company/header/index.tsx
@@ -1,35 +1,17 @@
 import React from 'react';
 import styles from "./index.module.css";
-import {FaBuilding, FaPlus, FaQuestionCircle} from "react-icons/fa";
-import Button from "@/app/components/button/button";
+import {FaBuilding} from "react-icons/fa";
 
-interface Props {
-    isShow: boolean;
-    onClickButton: () => void;
-}
-
-const HeaderCompany: React.FC<Props> = ({ isShow, onClickButton }) => {
+const HeaderCompany = () => {
     return (
         <div className={styles.container}>
 
             <div className={styles.left}>
                 <div className={styles.wrapperTitle}>
-                    <FaBuilding /> <h1 className={styles.title}>Dados da empresa / Clientes</h1> <FaQuestionCircle style={{ color: 'green' }} />
+                    <FaBuilding /> <h1 className={styles.title}>Empresa / Empregador</h1>
                 </div>
-                <h5 className={styles.subTitle}>Gerencie os grupos de empresas/cliente cadastrados em sua assinatura</h5>
+                <h5 className={styles.subTitle}>Gerencie as empresas e/ou empregadores cadastrados em sua assunatura</h5>
             </div>
-
-            {/* {!isShow && (
-                <div className={styles.right}>
-                    <Button
-                        title={'Cadastrar'}
-                        icon={FaPlus}
-                        background={'#31b331'}
-                        onClick={onClickButton}
-                    />
-                </div>
-            )} */}
-
         </div>
     );
 };
