d8b6ac0a9b6fa3a6ae6476a866f15243fd79540c
tiago <ti.almeida58@gmail.com>
2025-02-14 17:18:20 -0300

    refactor: adjusting and validation

--- src/app/api/company/route.ts
+++ src/app/api/company/route.ts
@@ -5,16 +5,27 @@ const prisma = new PrismaClient();
 
 export async function POST(req: Request) {
     try {
-        const data = await req.json(); // Convert the request body to JSON
+        const data = await req.json(); // Convert request body to JSON
+
+        // Validate received data
+        if (!data.name || !data.cnpj || !data.doctor || !data.registrationType || !data.fantasyName || !data.cnae || !data.cep || !data.esocialGroup) {
+            throw new Error("Campos obrigatórios estão faltando ou inválidos");
+        }
 
         const company = await prisma.company.create({
-            data,
+            data: {
+                ...data,
+                businessGroup: {
+                    connect: { id: data.businessGroup.id }
+                }
+            },
         });
 
         return NextResponse.json(company, { status: 201 });
-    } catch (error) {
+    } catch (error: any) {
+        console.error("Erro ao criar empresa:", error.message || error); // Log the error message
         return NextResponse.json(
-            { error: "Error creating company" },
+            { error: "Erro ao criar empresa" },
             { status: 500 }
         );
     }
@@ -24,7 +35,8 @@ export async function GET() {
     try {
         const companies = await prisma.company.findMany();
         return NextResponse.json(companies, { status: 200 });
-    } catch (error) {
+    } catch (error: any) {
+        console.error("Erro ao buscar empresas:", error.message || error); // Log the error message
         return NextResponse.json(
             { error: "Error fetching companies" },
             { status: 500 }
