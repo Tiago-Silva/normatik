3ec67a0165d3f2a2b70c41d053b39bd3a6b88ee2
tiago <ti.almeida58@gmail.com>
2025-05-16 14:56:27 -0300

    refactor: changing the header of the sector page to call the form

--- src/app/components/sector/header/index.tsx
+++ src/app/components/sector/header/index.tsx
@@ -1,33 +1,37 @@
 import React from 'react';
 import styles from "./index.module.css";
-import {FaBuilding} from "react-icons/fa";
+import {FaBuilding, FaPlus} from "react-icons/fa";
+import Button from "@/app/components/button/button";
 
 interface Props {
     isShow: boolean;
     onClickButton: () => void;
 }
 
-const HeaderSector = () => {
+const HeaderSector: React.FC<Props> = ({
+    isShow,
+    onClickButton,
+}) => {
     return (
         <div className={styles.container}>
 
             <div className={styles.left}>
                 <div className={styles.wrapperTitle}>
-                    <FaBuilding /> <h1 className={styles.title}>Empresa / Empregador</h1>
+                    <FaBuilding /> <h1 className={styles.title}>Setores/Departamentos</h1>
                 </div>
-                <h5 className={styles.subTitle}>Gerencie as empresas e/ou empregadores cadastrados em sua assunatura</h5>
+                <h5 className={styles.subTitle}>Gerencie os setores cadastrados em sua(s) empresa(s)</h5>
             </div>
 
-            {/*{!isShow && (*/}
-            {/*    <div className={styles.right}>*/}
-            {/*        <Button*/}
-            {/*            title={'Cadastrar'}*/}
-            {/*            icon={FaPlus}*/}
-            {/*            background={'#31b331'}*/}
-            {/*            onClick={onClickButton}*/}
-            {/*        />*/}
-            {/*    </div>*/}
-            {/*)}*/}
+            {!isShow && (
+                <div className={styles.right}>
+                    <Button
+                        title={'Cadastrar'}
+                        icon={FaPlus}
+                        background={'#31b331'}
+                        onClick={onClickButton}
+                    />
+                </div>
+            )}
         </div>
     );
 };
