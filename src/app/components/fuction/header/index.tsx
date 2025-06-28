29845a9a2ee0f77d88ead6b4ee22a8ef3f196abf
tiago <ti.almeida58@gmail.com>
2025-06-28 10:34:12 -0300

    feat: creating header function

---
+++ src/app/components/fuction/header/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from "./index.module.css";
+import {FaBuilding, FaPlus} from "react-icons/fa";
+import Button from "@/app/components/button/button";
+
+interface Props {
+    isShow: boolean;
+    onClickButton: () => void;
+}
+
+const HeaderFunction: React.FC<Props> = ({
+    isShow,
+    onClickButton,
+}) => {
+    return (
+        <div className={styles.container}>
+
+            <div className={styles.left}>
+                <div className={styles.wrapperTitle}>
+                    <FaBuilding /> <h1 className={styles.title}>Cargos/Funções</h1>
+                </div>
+                <h5 className={styles.subTitle}>Gerencie as funções cadastradas em sua(s) empresa(s)</h5>
+            </div>
+
+            {!isShow && (
+                <div className={styles.right}>
+                    <Button
+                        title={'Cadastrar'}
+                        icon={FaPlus}
+                        background={'#31b331'}
+                        onClick={onClickButton}
+                    />
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default HeaderFunction;
