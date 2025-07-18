2daec5362db031cf7e2952859a33bbcb5d8ad6f1
tiago <ti.almeida58@gmail.com>
2025-07-18 15:21:31 -0300

    feature: form validation

--- src/app/components/fuction/form/index.tsx
+++ src/app/components/fuction/form/index.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import styles from './index.module.css';
 import {FaFileAlt, FaSave} from "react-icons/fa";
 import Button from "@/app/components/button/button";
@@ -10,15 +10,15 @@ import {Company} from "@/app/interface/Company";
 import {zodResolver} from "@hookform/resolvers/zod";
 import InputZod from "@/app/components/input/InputZod";
 import Input from "@/app/components/input/input";
-import {NewSector, Sector, SectorDTO} from "@/app/interface/Sector";
-import {SectorService} from "@/app/service/SectorService";
+import {Function, FunctionDTO, NewFunction} from "@/app/interface/Function";
+import {FunctionService} from "@/app/service/FunctionService";
 
-export type sectorData = z.infer<typeof SectorDTO>;
+export type functionData = z.infer<typeof FunctionDTO>;
 
 interface Props {
     group?: BusinessGroup;
     company?: Company;
-    sector?: Sector;
+    func?: Function;
     onShowForm: () => void;
     onUpdateSectorListWhenSaving: () => void;
 }
@@ -26,78 +26,77 @@ interface Props {
 const FormFunction: React.FC<Props> = (
     {
         company,
-        sector,
+        func,
         onShowForm,
         onUpdateSectorListWhenSaving,
     }) => {
 
-    const {register, setValue, handleSubmit, formState: {errors, isValid}} = useForm<sectorData>({
-        resolver: zodResolver(SectorDTO),
+    const {register, setValue, handleSubmit, formState: {errors, isValid}} = useForm<functionData>({
+        resolver: zodResolver(FunctionDTO),
         mode: 'onChange',
         defaultValues: {
-            company: company
+            company: company,
+            code: 0
         }
     });
 
-    const handleCreateSector = async (data: sectorData) => {
-        const newSector: NewSector = {
-            name: data.nameRef,
+    const handleCreateFunction = async (data: functionData) => {
+        const newFunction: NewFunction = {
+            name: data.name,
             description: data.description,
-            nameRef: data.nameRef,
-            internalCode: data.internalCode,
+            code: data.code,
+            cbo: data.cbo,
             status: data.status || true,
-            sendDescription: data.sendDescription || false,
-            includeBuilding: data.includeBuilding || false,
             companyId: data.company.id,
             createdAt: new Date()
         }
 
-        const service = new SectorService();
+        const service = new FunctionService();
         try {
-            console.log('newSector: ', JSON.stringify(newSector));
-            await service.createSector(newSector);
+            console.log('newFunction: ', JSON.stringify(newFunction));
+            await service.createFunction(newFunction);
             onUpdateSectorListWhenSaving();
         } catch (error) {
-            console.error('Error creating sector', error);
+            console.error('Error creating function', error);
         }
     };
 
-    const handleEditSector = async (data: sectorData) => {
-        if (!company || !sector) {
-            throw new Error('Empresa ou setor não definidos');
-        }
+    // const handleEditSector = async (data: functionData) => {
+        // if (!company || !sector) {
+        //     throw new Error('Empresa ou setor não definidos');
+        // }
+        //
+        // const updateSector: Omit<Sector, 'company'> = {
+        //     ...sector,
+        //     ...data,
+        //     companyId: company.id,
+        //     updatedAt: new Date(),
+        // };
+        //
+        // const service = new SectorService();
+        // onUpdateSectorListWhenSaving();
+        // try {
+        //     await service.updateSector(updateSector);
+        // } catch (error) {
+        //     console.error('Error updating sector', error);
+        // }
+    // };
 
-        const updateSector: Omit<Sector, 'company'> = {
-            ...sector,
-            ...data,
-            companyId: company.id,
-            updatedAt: new Date(),
-        };
-
-        const service = new SectorService();
-        onUpdateSectorListWhenSaving();
-        try {
-            await service.updateSector(updateSector);
-        } catch (error) {
-            console.error('Error updating sector', error);
-        }
-    };
-
-    useEffect(() => {
-        if (sector) {
-            setValue('nameRef', sector.nameRef);
-            setValue('internalCode', sector.internalCode);
-            setValue('description', sector.description);
-            setValue('sendDescription', sector.sendDescription);
-            setValue('includeBuilding', sector.includeBuilding);
-            setValue('status', sector.status);
-        }
-    }, [sector, setValue]);
+    // useEffect(() => {
+    //     if (sector) {
+    //         setValue('nameRef', sector.nameRef);
+    //         setValue('internalCode', sector.internalCode);
+    //         setValue('description', sector.description);
+    //         setValue('sendDescription', sector.sendDescription);
+    //         setValue('includeBuilding', sector.includeBuilding);
+    //         setValue('status', sector.status);
+    //     }
+    // }, [sector, setValue]);
 
     return (
         <div className={styles.container}>
             <h2 className={styles.title}><FaFileAlt/> Dados da Função <span>*</span>:</h2>
-            <form onSubmit={handleSubmit(sector?.id ? handleEditSector : handleCreateSector)}>
+            <form onSubmit={handleSubmit(handleCreateFunction)}>
 
                 <Input
                     label={'Empresa:'}
@@ -107,8 +106,8 @@ const FormFunction: React.FC<Props> = (
                     }}
                 />
 
-                <InputZod label={'Nome/Referência:'} register={register('nameRef')} error={errors?.nameRef}/>
-                <InputZod label={'Código interno:'} register={register('code')} error={errors?.internalCode}/>
+                <InputZod label={'Nome/Referência:'} register={register('name')} error={errors?.name}/>
+                <InputZod label={'Código interno:'} register={register('code', { valueAsNumber: true })} error={errors?.code} type={'number'}/>
                 <InputZod label={'Descrição:'} register={register('description')} error={errors?.description}/>
                 <InputZod label={'C.B.O:'} register={register('cbo')} error={errors?.description}/>
 
@@ -116,13 +115,13 @@ const FormFunction: React.FC<Props> = (
                 <ToggleSwitchZod
                     label={'Status da Função:'}
                     name={'status'}
-                    isChecked={sector?.status || true}
+                    isChecked={func?.status || true}
                     setValue={setValue}
                     register={register('status')}
                 />
                 <div className={styles.wrapperButton}>
                     <Button
-                        title={sector?.id ? 'Alterar' : 'Salvar'}
+                        title={func?.id ? 'Alterar' : 'Salvar'}
                         icon={FaSave}
                         width={'250px'}
                         background={'#295A9C'}
