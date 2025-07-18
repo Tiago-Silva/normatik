85dc99efeed9cf772737be1bff7e1c9a3f65a7d2
tiago <ti.almeida58@gmail.com>
2025-07-18 15:21:14 -0300

    feature: creating FunctionDTO for ZOD

--- src/app/interface/Function.ts
+++ src/app/interface/Function.ts
@@ -1,4 +1,5 @@
 import {Company} from "@/app/interface/Company";
+import {z} from "zod";
 
 
 export interface Function {
@@ -20,6 +21,7 @@ export interface NewFunction {
     code: number;
     cbo: string;
     description: string;
+    status: boolean;
     createdAt: Date;
     companyId: number;
 }
@@ -27,14 +29,14 @@ export interface NewFunction {
 export interface SearchFilters {
     company: Company;
     name: string;
-    code: string;
+    code: number;
     status: boolean;
 }
 
 export interface SearchActions {
     onSetCompany: (company: Company) => void;
     onSetName: (name: string) => void;
-    onSetCode: (code: string) => void;
+    onSetCode: (code: number) => void;
     onSelectStatus: (status: boolean) => void;
     onSearchFunctions: (companyId: number, status: boolean) => void;
 }
@@ -43,3 +45,14 @@ export interface SearchFunctionProps {
     filters: SearchFilters;
     actions: SearchActions;
 }
+
+export const FunctionDTO = z.object({
+    company: z.object({
+        id: z.number({ required_error: "O ID da função é obrigatório"}),
+    }),
+    name: z.string().min(4, { message: "O nome da função deve ter pelo menos 4 caracteres" }),
+    code: z.number({ required_error: "O código interno é obrigatório" }),
+    description: z.string().min(4, { message: "A descrição deve ter pelo menos 4 caracteres" }),
+    cbo: z.string().min(2, { message: "O C.B.O deve ter pelo menos 2 caractreres" }),
+    status: z.boolean().optional(),
+});
