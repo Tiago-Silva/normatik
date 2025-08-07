ec125bdcd08e33043f5fb10607ba2b8ceb568765
tiago <ti.almeida58@gmail.com>
2025-08-07 14:57:52 -0300

    refactor: adding and using name ande code in parameters

--- src/app/api/function/search/route.ts
+++ src/app/api/function/search/route.ts
@@ -7,28 +7,43 @@ export async function GET(req: Request) {
     const {searchParams} = new URL(req.url);
     const companyId = searchParams.get('companyId');
     const status = searchParams.get('status') === 'true';
+    const functionName = searchParams.get('functionName');
+    const internalCode = searchParams.get('internalCode');
 
     if (companyId) {
         try {
+            const whereConditions: Record<string, unknown> = {
+                companyId: parseInt(companyId, 10),
+                status: status,
+            };
+
+            if (functionName && functionName.trim() !== '') {
+                whereConditions.name = {
+                    contains: functionName.toLowerCase(),
+                };
+            }
+
+            if (internalCode && !isNaN(parseInt(internalCode, 10)) && internalCode != '0') {
+                whereConditions.code = parseInt(internalCode, 10);
+            }
+
             const functions = await prisma.function.findMany({
-                where: {
-                    companyId: parseInt(companyId, 10),
-                    status: status,
-                },
+                where: whereConditions,
             });
-            return NextResponse.json(functions, { status: 200 });
-        } catch (error: any) {
-            console.error("Error ao buscar funções:", error.message || error);
+
+            return NextResponse.json(functions, {status: 200});
+        } catch (error: unknown) {
+            const errorMessage = error instanceof Error ? error.message : 'Erro desconhecido';
+            console.error("Error ao buscar funções:", errorMessage);
             return NextResponse.json(
-                { error: "Error ao buscar funções" },
-                { status: 500 }
+                {error: "Error ao buscar funções"},
+                {status: 500}
             );
         }
     } else {
         return NextResponse.json(
-            { error: "Parâmetros de consulta inválidos" },
-            { status: 400 }
+            {error: "Parâmetros de consulta inválidos"},
+            {status: 400}
         );
     }
-
 }
